92226ea21caf5b980aed0ecfe915e846536fbbfb
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-24 18:12:22 +0300

    Implements basic websocket server
    Sets up a websocket server using socket.io with CORS configuration for communication with a client hosted on 'http://localhost:3000'.
    
    Includes basic message handling to receive messages from the client and log them to the console.  Updates the server port to 3010 and adds a simple response to the root route.

--- src/app.ts
+++ src/app.ts
@@ -6,18 +6,28 @@ import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+    },
+});
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get('/', (req, res) => {
+    res.send('Hello, It`s a WS server');
     res.sendFile(join(__dirname, 'index.html'));
 });
 
-io.on('connection', (socket) => {
+io.on('connection', (socketChannel) => {
     console.log('a user connected');
+    socketChannel.on('client-message-sent', (message: string) => {
+        console.log(message);
+    });
 });
 
-server.listen(3000, () => {
-    console.log('server running at http://localhost:3000');
+server.listen(3010, () => {
+    console.log('server running at http://localhost:3010');
 });
