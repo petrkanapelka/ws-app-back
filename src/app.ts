7abdb0cc21a8d29feb04f61bff5e8b0fde150f1d
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-24 19:31:55 +0300

    Adds initial messages to the WebSocket server
    Introduces an array of initial messages and sends them to the client upon connection.  This provides the client with initial conversation history.

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,26 @@ import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
 
+type User = {
+    id: string;
+    name: string;
+};
+
+type Message = {
+    id: string;
+    message: string;
+    user: User;
+};
+
+const messages: Message[] = [
+    { message: 'Hello, Viktor', id: '23f2332', user: { id: 'sddsdsfds', name: 'Dimych' } },
+    { message: 'Hello, Dimych', id: '23fd32c23', user: { id: 'eefw2', name: 'Viktor' } },
+    { message: 'Hello, Petrovich', id: '23fd32c23', user: { id: 'eefw2', name: 'Petro' } },
+    { message: 'Hello, Viktor', id: '23f2332', user: { id: 'sddsdsfds', name: 'Dimych' } },
+    { message: 'Hello, Dimych', id: '23fd32c23', user: { id: 'eefw2', name: 'Viktor' } },
+    { message: 'Hello, Petrovich', id: '23fd32c23', user: { id: 'eefw2', name: 'Petro' } },
+];
+
 const app = express();
 const server = createServer(app);
 
@@ -18,7 +38,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get('/', (req, res) => {
     res.send('Hello, It`s a WS server');
-    res.sendFile(join(__dirname, 'index.html'));
+    // res.sendFile(join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socketChannel) => {
@@ -26,6 +46,8 @@ io.on('connection', (socketChannel) => {
     socketChannel.on('client-message-sent', (message: string) => {
         console.log(message);
     });
+
+    socketChannel.emit('init-messages-published', messages);
 });
 
 server.listen(3010, () => {
